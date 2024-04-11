b46be80b1a89656d81775cba76c56d2573bca5f8
Viv696969 <chouhanvivek207@gmail.com>
2024-04-11 21:17:50 +0530

    optimized and cleaned code

--- Movie Ticket Booking/main.py
+++ Movie Ticket Booking/main.py
@@ -5,16 +5,37 @@ from pprint import pprint
 import re
 # from time import time
 import mysql.connector as mysql
+import smtplib
 
-# def connect():
-conn=mysql.connect(
-    host='localhost',
-    user='root',
-    password='varad1901',
-    database='cinema_mitra_db'
-)
-cursor=conn.cursor()
-    # return cursor,conn
+def connect():
+    conn=mysql.connect(
+        host='localhost',
+        user='root',
+        password='varad1901',
+        database='cinema_mitra_db'
+    )
+    cursor=conn.cursor()
+    return conn,cursor
+
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("vivek.211215.co@mhssce.ac.in", "varad1901")
+
+
+def give_seat_nos(seats,section,position):
+    seat_nums=""
+    if section=="Balcony":
+        for i in range(seats):
+            seat_nums+=f"H{i+1} , "
+    else:
+        for i in range(seats):
+            seat_nums+=f"C{i+1},"
+    return seat_nums
+
+def send_mail(reciever,mssg):
+    
+    s.sendmail("vivek.211215.co@mhssce.ac.in", reciever, mssg)
+
 
 app=FastAPI()
 
@@ -51,6 +72,15 @@ async def webhook(request:Request):
         
         case "take_position":
             return  take_position(parameters,session_id)
+        
+        case "take_no_of_seats":
+            return take_no_of_seats(parameters,session_id)
+        
+        case "book_ticket":
+            return book_ticket(session_id)
+        
+        case "take_gmail":
+            return take_email(parameters,session_id)
     
 
 
@@ -59,6 +89,7 @@ def give_session_id(output_context):
     return session_id
 
 def show_movies():
+    conn,cursor=connect()
     query="Select name from movies;"
     cursor.execute(query)
     movies=""
@@ -67,15 +98,14 @@ def show_movies():
         movies+=i[0]+" ,"
 
     fulfillmentText=f"We have {movies} streaming today.Which one would you like to ask?"
-    return JSONResponse(
-        {
-            'fulfillmentText':fulfillmentText
-        }
-    )
+    cursor.close()
+    conn.close()
+    return create_response(fulfillmentText)
      
 def new_booking(session_id):
     user_bookings[session_id]={}
     query="Select name from movies;"
+    conn,cursor=connect()
     cursor.execute(query)
     movies=""
 
@@ -83,18 +113,14 @@ def new_booking(session_id):
         movies+=i[0]+" ,"
     print(user_bookings)
     fulfillmentText=f"We have {movies} streaming today.Which one would you like to ask?"
-    return JSONResponse(
-        {
-            'fulfillmentText':fulfillmentText
-        }
-    )
+    cursor.close()
+    conn.close()
+    return create_response(fulfillmentText)
 
 def take_movie(parameters,session_id):
     movie_name=parameters['movie_name']
-    print(movie_name)
     user_bookings[session_id]['movie_name']=movie_name
-    print(user_bookings)
-
+    conn,cursor=connect()
     query=f'''
     select time 
     from movies join show_times_table
@@ -108,22 +134,12 @@ def take_movie(parameters,session_id):
         timings+=i[0]+","
     user_bookings[session_id]['timings']=timing_list
     fulfillmentText=f"Selected {movie_name} successfully.The available show times are {timings}.Please select any one from that"
-    return JSONResponse(
-        {
-            'fulfillmentText':fulfillmentText
-        }
-    )
+    cursor.close()
+    conn.close()
+    return create_response(fulfillmentText)
 
 def take_time(time,session_id):
-    print(time)
     movie=user_bookings[session_id]['movie_name']
-    # query=f'''
-    # select time 
-    # from movies join show_times_table
-    # on movies.id=show_times_table.movie_id
-    # where name='{movie}';
-    # '''
-    # cursor.execute(query)
     is_present=False
     timings=""
     for i in user_bookings[session_id]['timings']:
@@ -132,72 +148,99 @@ def take_time(time,session_id):
         if time==i[0]:
             is_present=True
 
-    print(is_present)
     if not is_present:
-        return JSONResponse(
-        {
-            'fulfillmentText':f"We have no such show time for {movie}..Please select from {timings} "
-        }
-                            )
+        return create_response(f"We have no such show time for {movie}..Please select from {timings} ")
     user_bookings[session_id]['show_time']=time
     print(user_bookings)
     fulfillmentText=f"Okkies..Selected {time} for {movie} for you..Please now select the section from normal and balcony"
-    return JSONResponse(
-        {
-            'fulfillmentText':fulfillmentText
-        }
-    )
+    return create_response(fulfillmentText)
 
 def take_section(parameters,session_id):
     if session_id not in user_bookings:
-        return JSONResponse(
-        {
-            'fulfillmentText':"Please start a new booking first..."
-        }
-    )
+        return create_response("Please start a new booking first...")
     if "movie_name" not in user_bookings[session_id]:
-        return JSONResponse(
-        {
-            'fulfillmentText':"Please select a movie first...For eg Avengers"
-
-        }
-    )
+        return create_response("Please select a movie first...For eg Avengers")
     if "show_time" not in user_bookings[session_id]:
-        return JSONResponse(
-        {
-            'fulfillmentText':f"Please select a show time first for {user_bookings[session_id]['movie_name']} from {user_bookings[session_id]['timings']}"
-
-        }
-    )
+        return create_response(f"Please select a show time first for {user_bookings[session_id]['movie_name']} from {user_bookings[session_id]['timings']}")
     section=parameters['section']
-    print(section)
     user_bookings[session_id]['section']=section
-    return JSONResponse(
-        {
-            'fulfillmentText':f"Selected {user_bookings[session_id]['movie_name']} at {user_bookings[session_id]['show_time']} in section {section}..Please now select the position you would like to sit in from left, middle, right"
-        }
-    )
+    fulfillmentText=f"Selected {user_bookings[session_id]['movie_name']} at {user_bookings[session_id]['show_time']} in section {section}..Please now select the position you would like to sit in from left, middle, right"
+    return create_response(fulfillmentText)
     
 def take_position(parameters,session_id):
     if session_id not in user_bookings:
-        return JSONResponse(
-        {
-            'fulfillmentText':"Please start a new booking first Eg=>(I want to start a new booking)"
-        }
-    )
+        return create_response("Please start a new booking first Eg=>(I want to start a new booking)")
     position=parameters['position']
     if position not in positions:
-          return JSONResponse(
-        {
-            'fulfillmentText':"Please enter a valid postion from left,right or middle.."
-        }
-    )
+          return create_response("Please enter a valid postion from left,right or middle..")
     
     user_bookings[session_id]['position']=position
     fulfillmentText=f'''Selected {position} in {user_bookings[session_id]['section']} for {user_bookings[session_id]['movie_name']} at {user_bookings[session_id]["show_time"]}.Now please Enter the number of seats..Max Limit=4'''
-    return JSONResponse(
-        {
-            'fulfillmentText':fulfillmentText
-        }
-    )
+    return create_response(fulfillmentText)
     
+def take_no_of_seats(parameters,session_id):
+    if session_id not in user_bookings:
+
+        return create_response("Please start a new booking first...")
+    seats=int(parameters['number'])
+
+    if seats>4 or seats<0:
+        return create_response("Please enter valid number of seats from 1,2,3,4")
+    
+    user_bookings[session_id]['no_of_seats']=seats
+    fulfillmentText=f'''Selected {seats} seats for {user_bookings[session_id]['movie_name']} at {user_bookings[session_id]['show_time']} in section {user_bookings[session_id]['section']} and position {user_bookings[session_id]['position']} Successfully...Should i confirm your booking ?'''
+    return create_response(fulfillmentText)
+
+def book_ticket(session_id):
+    print(user_bookings[session_id])
+    user_bookings[session_id]['confirmed']=True
+    fulfillmentText="Please Enter your gmail to finalize your booking...eg=>abcd@gmail.com"
+    return create_response(fulfillmentText)
+    
+def take_email(parameters,session_id):
+    conn,cursor=connect()
+    email=parameters['email']
+    user=user_bookings[session_id]
+    seats=user["no_of_seats"]
+    section=user['section']
+    position=user['position']
+    seat_nums=give_seat_nos(seats,section,position)
+    movie=user['movie_name']
+    showtime=user['show_time']
+    query="select max(ticket_id) from confirmed_bookings;"
+    cursor.execute(query)
+    id=cursor.fetchone()[0]
+    ticket_id=id+1
+    email_mssg=f'''
+    Thank you for booking {movie} at {showtime}
+    Section : {section}
+    Seat numbers : {seat_nums}
+    Number of tickets : {seats}
+    Your Ticket id is {ticket_id}
+    '''
+    s.sendmail("vivek.211215.co@mhssce.ac.in", email, email_mssg)
+    query=f'''
+    insert into confirmed_bookings values(
+    {email},
+    {seats},
+    {section}
+    ,{position},
+    "{movie}",
+    "{showtime}",
+    "12:34",
+    {seat_nums},
+    {ticket_id}
+    );
+    '''
+    cursor.execute(query)
+    fulfillmentText=f"{email} thank you for booking {movie} with us..Your ticket id is {ticket_id}..Incase you forget your ticket id check you mail once..Enjoy"
+    cursor.close()
+    conn.close()
+    return create_response(fulfillmentText)
+
+def create_response(fulfillmentText):
+    return JSONResponse({'fulfillmentText': fulfillmentText})
+
+
+
+
